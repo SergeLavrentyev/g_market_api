166db515a74fd001c2910b3b7e752b076f8e91bf
Sergey <53293773+SergeyLavrentyev@users.noreply.github.com>
2021-06-28 21:27:30 +0400

    change update time

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -26,7 +26,7 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule)
     {
         $schedule->command('ticker:download')->everyMinute();
-        $schedule->command('catalog:get')->hourly();
+        $schedule->command('catalog:get')->everyMinute();
     }
 
     /**
